8730d4602e61a1893495f9eda768a29c6356a7cf
clutchyminds <thomas@mnlewis.com>
2026-02-02 12:53:12 +0100

    maj attaque et map

--- utils.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-def load_img(path, scale):
-    try:
-        img = pygame.image.load(path).convert_alpha()
-        return pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-    except:
-        s = pygame.Surface((64, 64)); s.fill((255, 0, 50)); return s
